e370a72759ccd206d2b3447a42db3ad4983b2dde
Ivanov <ramireztiradojose@gmail.com>
2020-09-15 19:27:31 -0700

    Updates

---
+++ precache-manifest.a1f949556bd3d028561e93304134151c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b1a2bbf90746b4e4931c2f38eb613d",
+    "url": "/StarWarsMiniPage/index.html"
+  },
+  {
+    "revision": "aff4a3209ff7be474bf3",
+    "url": "/StarWarsMiniPage/static/css/main.9d2b6c25.chunk.css"
+  },
+  {
+    "revision": "02fb85b568ebf853d02e",
+    "url": "/StarWarsMiniPage/static/js/2.d127447f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/StarWarsMiniPage/static/js/2.d127447f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aff4a3209ff7be474bf3",
+    "url": "/StarWarsMiniPage/static/js/main.d4992f7f.chunk.js"
+  },
+  {
+    "revision": "04aa554e69c111f067e1",
+    "url": "/StarWarsMiniPage/static/js/runtime-main.b4b3bc02.js"
+  }
+]);
